129102a8447042bb155cc78bea19b46ba5dfcd52
harish8608 <justinharish04@gmail.com>
2023-04-05 09:56:50 +0530

    learn new

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import SubmitField
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = r"C:\learning-platform\upload documents"
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), r"C:\learning-platform\upload documents")
+app.config['SECRET_KEY'] = 'your_secret_key'
 
-@app.route('/')
+class UploadForm(FlaskForm):
+    file = FileField('Upload File', validators=[FileRequired(), FileAllowed(['pdf', 'doc', 'docx', 'txt'], 'Only PDF, DOC, DOCX, and TXT files allowed!')])
+    submit = SubmitField('Upload')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = UploadForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
     files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('index.html', files=files)
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    file = request.files['file']
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-    return redirect(url_for('index'))
+    return render_template('index.html', form=form, files=files)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
